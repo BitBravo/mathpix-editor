2582887bfc8a92f9ce3e8765228c6bcacd7a8e7f
javascript603 <react.dev@yahoo.com>
2018-10-23 19:39:48 +0200

    removed non-useful library

--- app/components/Preview/Preview.js
+++ app/components/Preview/Preview.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Parser from 'html-react-parser';
 import hljs from 'highlight.js';
