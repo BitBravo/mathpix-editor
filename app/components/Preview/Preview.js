e58a6888a13f733a6cb14a8c4b45ff88db404299
dream222 <dream.dev1025@gmail.com>
2018-11-10 04:12:47 +0100

    removed unuseless module

--- app/components/Preview/Preview.js
+++ app/components/Preview/Preview.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Parser from 'html-react-parser';
-import ReactDOM from 'react-dom';
 import hljs from 'highlight.js';
 import './style.scss';
 
